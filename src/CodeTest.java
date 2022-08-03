3dd0cae30d5398d11411986033b957e04696c126
Leo Fabricio <leofabr3@gmail.com>
2022-08-03 13:48:50 -0300

    main implement

--- src/CodeTest.java
+++ src/CodeTest.java
@@ -1,5 +1,14 @@
 public class CodeTest {
 
+  public static void main(String[] args) {
+
   //inheritance : is used to implement the "is-a" relationship
   //composition : is used to implement the "has-a" relationship
+
+  Point center = new Point(5,5);
+  Circle c = new Circle(center,10);
+
+  System.out.println(c.CalculateArea());
+  }
+
 }
