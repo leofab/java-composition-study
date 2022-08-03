91b42e72746e833e31a94b85176f176011925232
Leo Fabricio <leofabr3@gmail.com>
2022-08-03 13:38:05 -0300

    first commit

---
+++ src/Point.java
@@ -0,0 +1,27 @@
+public class Point {
+
+  int x;
+  int y;
+
+  // Constructor
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX(){
+    return x;
+  }
+
+  public void setX(){
+    this.x = x;
+  }
+
+  public int getY(){
+    return y;
+  }
+
+  public void setY(){
+    this.y = y;
+  }
+}
